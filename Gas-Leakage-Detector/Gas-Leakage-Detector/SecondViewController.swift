bc5bd7b7f1855fea934e392426a0ca1c2fa14e97
Rohit Saidugari <Rohith.saidugari@outlook.com>
2020-02-27 21:55:54 -0600

    initial commit

---
+++ Gas-Leakage-Detector/Gas-Leakage-Detector/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Gas-Leakage-Detector
+//
+//  Created by Student on 2/27/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
