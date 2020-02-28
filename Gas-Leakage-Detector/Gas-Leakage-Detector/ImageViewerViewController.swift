db712f61d23929f3bf58d2ed809c46dfb35702f4
Pranay Allikanti <pranaykumar0987@gmail.com>
2020-02-27 22:23:56 -0600

    Created a view controller file named "ImageViewerViewController"

---
+++ Gas-Leakage-Detector/Gas-Leakage-Detector/ImageViewerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewerViewController.swift
+//  Gas-Leakage-Detector
+//
+//  Created by Pranay Allikanti on 27/02/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     navigationItem.title = "Image Viewer"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
